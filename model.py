33a450314c1cd87213f604ba7406a9dbe298d71c
jwerr <shivanisha456987@gmail.com>
2024-04-16 23:10:55 +0530

    added 3 - new ml model code

--- model.py
+++ model.py
@@ -1,1 +1,11 @@
 print("Addind all ml related code")
+
+
+def model1():
+    print("Adding code for ML Model1")
+
+def model2():
+    print("Adding code for ML Model2")
+
+def model3():
+    print("Adding code for ML Model3")
